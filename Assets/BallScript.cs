27b1cf3f99f6282c6cf9a82d606d3af7453e8ff5
semih9 <61362582+semih9@users.noreply.github.com>
2020-09-13 18:17:39 +0300

    Part 1 - Part 2
    Part 1 paddle movement - Done!
    Part 2 ball movement - Done!

---
+++ Assets/BallScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public bool inPlay;
+    public Transform paddle;
+    public float ballSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!inPlay)
+        {
+            transform.position = paddle.position;
+        }
+
+        if (Input.GetButtonDown("Jump") && !inPlay)
+        {
+            inPlay = true;
+            rb.AddForce(Vector2.up * ballSpeed);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bottom"))
+        {
+            Debug.Log("Ball hit the bottom of the screen!");
+            rb.velocity = Vector2.zero;
+            inPlay = false;
+        }
+    }
+}
